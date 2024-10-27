d3c57730eea1c96cc7fdbb1caa9e5f95f9ffd55e
Erdem <earda230@gmail.com>
2024-10-27 19:21:37 +0300

    Crud operations added and Listing with using bootstrap library

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,39 +1,88 @@
-import { useEffect ,useState } from 'react' 
-import './App.css'
-import axios from 'axios'; 
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BASE_URL = "http://localhost:8080" ;
 
 function App() { 
   const [users, setUsers] = useState([]);
-  const getAllUsers= async()=>{ 
+  const [sortOrder, setSortOrder] = useState("asc");
+
+   const getAllUsers= async()=>{ 
       const response = await axios.get(BASE_URL + "/user");
       setUsers(response.data);  
-  }
-/*  const getElementById= async(userId)=>{
-    const response = await axios.get(`${BASE_URL}/user/${userId}`);
-    setUsers( response.data);
   } 
-  const getById = async()=>{
-    const response = await getElementById(2);
-    console.log(response.data);
-  }*/
+   const getUserById= async(userId)=>{
+    const response = await axios.get(`${BASE_URL}+/user/+${userId}`);
+    setUsers(response.data);
+  }  
+
+  const createUser = async(newUser)=>{
+    await axios.post(`${BASE_URL}/user`,newUser);
+  }
 
   
   useEffect(()=>{// component ilk yuklendiginde methodun icindeki fonksiyonu calistir.
     getAllUsers(); 
+   // getUserById(1);
+    /*const newUser={
+        "id": 4,
+        "name": "arda",
+        "email": "ardart@gmail.com",
+        "age": 45
+    }
+    createUser(newUser);*/
   },[])
+  const sortById  = () => {
+    const sortedUsers = [...users].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.id - b.id;
+      } else {
+        return b.id-a.id;
+      }
+    });
+    setUsers(sortedUsers);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+  const headerStyle = {
+    backgroundColor: "#4CAF50",
+    color: "white",
+    padding: "8px",
+  };
 
+  const cellStyle = {
+    padding: "8px",
+    border: "1px ",
+    textAlign: "left",
+  };
   return (
    
-      <div>
-       <h1>Users</h1>
-       <ul>
+    <div className="container">
+    <h1 className="my-4">Users Table</h1>
+    <button className="btn btn-primary mb-3" onClick={sortById}>
+        ID'ye Göre Sırala ({sortOrder === "asc" ? "Ascending" : "Descending"})
+      </button>
+    <table className="table table-hover">
+      <thead className="thead-dark">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li> // Kullanıcı isimlerini liste halinde gösteriyoruz.
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
         ))}
-      </ul>
-      </div> 
+      </tbody>
+    </table>
+  </div>
   )
 }
  
